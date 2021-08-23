7c207f5132bcd63e0e5a8eff81d1cf9d25494fb4
WoozyDragon <woozydragon@gmail.com>
2021-08-23 18:02:51 -0500

    work in progress -> making it on a clock

--- FactorClock.cpp
+++ FactorClock.cpp
@@ -8,103 +8,28 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <vector>
+#include <thread>
 
-class Timer
+std::vector<long long> factor(long long input)
 {
-private:
-    // Type aliases to make accessing nested type easier
-    using clock_t = std::chrono::high_resolution_clock;
-    using second_t = std::chrono::duration<double, std::ratio<1>>;
-
-    std::chrono::time_point<clock_t> m_beg;
-
-public:
-    Timer() : m_beg(clock_t::now())
+    std::vector<long long> factors{};
+    while (input != 1)
     {
-    }
-
-    void reset()
-    {
-        m_beg = clock_t::now();
-    }
-
-    double elapsed() const
-    {
-        return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
-    }
-};
-
-int main()
-{
-    /*
-    //defines if file is just 1 million or all 10 million
-    //false == just 1 million
-    //true == all 10 million
-
-    bool primeFile{ false };
-
-    //name of the prime file to use
-    std::string primeFileName{};
-
-    if (primeFile)
-    {
-        //primes up to 10 million
-        primeFileName = "primes1.txt";
-
-    }
-    else
-    {
-        //primes up to 1 million
-        primeFileName = "primes2.txt";
-    }
-
-    std::string line;
-    std::ifstream myfile(primeFileName);
-        while (getline(myfile, line))
-    {
-        std::cout << line << '\n';
-    }
-    */
-
-    std::time_t t2 = std::time(0);
-
-    long long bigNumber{t2};
-    long long userInput{};
-    
-    std::cout << "Enter a positive integer. Enter 0 for default (current unix time)";
-    std::cin >> userInput;
-    if (userInput < 0)
-    {
-        std::cout << "Unable to factor negative numbers at this time "
-            << "please input a postive integer\n";
-        return -1;
-    }
-    if (userInput != 0)
-    {
-        bigNumber = userInput;
-    }
-
-    std::cout << "Input = " << bigNumber << '\n';
-
-    Timer t;
-    //start of timing
-
-    while (bigNumber != 1)
-    {
-        if ((bigNumber % 2) == 0)
+        if ((input % 2) == 0)
         {
-            std::cout << "  " << 2 << '\n';
-            bigNumber /= 2;
+            factors.push_back(2);
+            input /= 2;
         }
         else
         {
             long long testNumber{ 3 };
             while (true)
             {
-                if ((bigNumber % testNumber) == 0)
+                if ((input % testNumber) == 0)
                 {
-                    std::cout << "  " << testNumber << '\n';
-                    bigNumber /= testNumber;
+                    factors.push_back(testNumber);
+                    input /= testNumber;
                     break;
                 }
                 else
@@ -114,15 +39,56 @@ int main()
             }
         }
     }
-    std::cout << "done\n";
-    
-    //end of timing
-    std::cout << "Time elapsed: " << t.elapsed() << " seconds.\n";
+    return factors;
+}
 
-    //Just wait for the input. This is a dummy variable used to make the program
-    //wait for the execution to finish
-    int justWaitForTheInput;
-    std::cin >> justWaitForTheInput;
+std::vector<long long> values[20];
+int times[20];
+long long bigNumber{ std::time(0) };
+int c_arrayIndex{ 0 };
+
+void newFactor()
+{  
+    if (times[c_arrayIndex] >= std::time(0) + 19)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    }
+
+    times[c_arrayIndex] = bigNumber;
+    values[c_arrayIndex] = factor(bigNumber);
+
+
+    ++c_arrayIndex;
+    if (c_arrayIndex >= 20) c_arrayIndex = 0;
+
+    ++bigNumber;
+}
+
+void printFactors() {
+    auto start = std::chrono::system_clock::now();
+
+    auto c_time = std::time(0);
+    
+    int array_index{ 0 };
+
+    for (int i = 0; )
+
+
+    std::cout << "Current Time is " << c_time << '\n';
+
+    auto end = std::chrono::system_clock::now();  
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000)-(end - start));
+}
+
+
+
+int main()
+{
+
+    while (true)
+    {
+
+    }
 
     return 0;
 }
