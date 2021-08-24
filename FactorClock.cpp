acc6dd4f235d930573dda02f164084f1edfeba04
WoozyDragon <woozydragon@gmail.com>
2021-08-23 22:11:57 -0500

    First working prototype of multithreaded work

--- FactorClock.cpp
+++ FactorClock.cpp
@@ -9,6 +9,8 @@
 #include <string>
 #include <ctime>
 #include <vector>
+#include <algorithm>
+
 #include <thread>
 
 std::vector<long long> factor(long long input)
@@ -49,35 +51,42 @@ int c_arrayIndex{ 0 };
 
 void newFactor()
 {  
-    if (times[c_arrayIndex] >= std::time(0) + 19)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    while (true) {
+        if (times[c_arrayIndex] >= std::time(0) + 18)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+        }
+
+        times[c_arrayIndex] = bigNumber;
+        values[c_arrayIndex] = factor(bigNumber);
+
+
+        ++c_arrayIndex;
+        if (c_arrayIndex >= 20) c_arrayIndex = 0;
+
+        ++bigNumber;
     }
-
-    times[c_arrayIndex] = bigNumber;
-    values[c_arrayIndex] = factor(bigNumber);
-
-
-    ++c_arrayIndex;
-    if (c_arrayIndex >= 20) c_arrayIndex = 0;
-
-    ++bigNumber;
 }
 
 void printFactors() {
-    auto start = std::chrono::system_clock::now();
+    while (true) {
+        auto start = std::chrono::system_clock::now();
 
-    auto c_time = std::time(0);
-    
-    int array_index{ 0 };
+        auto c_time = std::time(0);
 
-    for (int i = 0; )
+        int x = std::distance(times, std::find(times, times + 20, c_time));
 
+        std::cout << "Current Time is " << times[x] << '\n';
+        std::cout << "Factors are : \n";
 
-    std::cout << "Current Time is " << c_time << '\n';
+        for (auto singleFactor : values[x])
+        {
+            std::cout << " " << singleFactor << '\n';
+        }
 
-    auto end = std::chrono::system_clock::now();  
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000)-(end - start));
+        auto end = std::chrono::system_clock::now();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000) - (end - start));
+    }
 }
 
 
@@ -85,10 +94,11 @@ void printFactors() {
 int main()
 {
 
-    while (true)
-    {
+    std::thread calculations(newFactor);
+    std::thread output(printFactors);
 
-    }
+    calculations.join();
+    output.join();
 
     return 0;
 }
