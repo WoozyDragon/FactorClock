5f166cdae4b1b655d42213293bbf251eea818a68
woozydragon <woozydragon@gmail.com>
2021-08-27 22:17:38 -0500

    Got initial window up and running

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -1,9 +1,63 @@
 #include <Windows.h>
+#include "platform_common.cpp"
+
+typedef unsigned int u32;
+
+struct RenderState {
+	int width;
+	int height;
+
+	void* memory;
+
+	BITMAPINFO bitmapinfo;
+};
 
 bool running{ true };
+static RenderState renderstate;
 
 LRESULT CALLBACK window_callback(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam) {
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		LRESULT result{ 0 };
+
+		switch (uMsg)
+		{
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+			break;
+		}
+
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			renderstate.width = rect.right - rect.left;
+			renderstate.height = rect.bottom - rect.top;
+
+			int buffer_size{ renderstate.width * renderstate.height * static_cast<int>(sizeof(u32)) };
+
+			if (renderstate.memory) {
+				VirtualFree(renderstate.memory, 0, MEM_RELEASE);
+			}
+
+			renderstate.memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			renderstate.bitmapinfo.bmiHeader.biSize = sizeof(renderstate.bitmapinfo.bmiHeader);
+			renderstate.bitmapinfo.bmiHeader.biHeight = renderstate.height;
+			renderstate.bitmapinfo.bmiHeader.biWidth = renderstate.width;
+			renderstate.bitmapinfo.bmiHeader.biPlanes = 1;
+			renderstate.bitmapinfo.bmiHeader.biBitCount = 32;
+			renderstate.bitmapinfo.bmiHeader.biCompression = BI_RGB;
+
+
+			break;
+		}
+
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+
+	}
+	return result;
 }
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR lpCmdLine, int nShowCmd) {
@@ -23,6 +77,63 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR lpCmdLine, int nS
 	HDC hdc = GetDC(window);
 
 	while (running) {
-		;
+		//get inputs
+		MSG message;
+
+		Input input = {};
+
+		float deltaTime = 0.01666f;
+		LARGE_INTEGER frameBegin;
+		QueryPerformanceCounter(&frameBegin);
+
+		float performanceFrequency;
+		{
+			LARGE_INTEGER perf;
+			QueryPerformanceFrequency(&perf);
+			performanceFrequency = (float)perf.QuadPart;
+		}
+
+		for (int i = 0; i < BUTTON_COUNT; ++i) {
+			input.buttons[i].changed = false;
+		}
+
+
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+				switch (message.message)
+				{
+				case WM_KEYUP:
+				case WM_KEYDOWN: {
+					u32 vk_code = (u32)message.wParam;
+					bool is_down = ((message.lParam & (1 << 31)) == 0);
+
+#define processButton(b, vk)\
+case vk:{\
+	input.buttons[b].isDown = is_down;\
+	input.buttons[b].changed = true;\
+} break;\
+
+					switch (vk_code)
+					{
+						processButton(BUTTON_UP, VK_UP);
+						processButton(BUTTON_DOWN, VK_DOWN);
+						processButton(BUTTON_W, 'W');
+						processButton(BUTTON_S, 'S');
+						processButton(BUTTON_LEFT, VK_LEFT);
+						processButton(BUTTON_RIGHT, VK_RIGHT);
+						processButton(BUTTON_ENTER, VK_RETURN);
+
+					case 'Q':
+						running = false;
+						break;
+					}
+				} break;
+
+				default: {
+					TranslateMessage(&message);
+					DispatchMessage(&message);
+				} break;
+			}
+
+		}
 	}
 }
